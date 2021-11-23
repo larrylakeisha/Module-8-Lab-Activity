47cbcac86df262438d22f98ad20097e0448b641a
larrylakeisha <91972138+larrylakeisha@users.noreply.github.com>
2021-11-23 17:53:03 -0600

    Update Print value.py

--- Print value.py
+++ Print value.py
@@ -3,4 +3,10 @@
 
 # This program takes a list and prints if the value is 5 is in the list
 
-def List_of_five():
+def printValue(value, list):
+    for i in list:
+        if i == value:
+            ans = "The value is five."
+            return ans
+
+print(printValue(5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
