e0103dd3a01d4b440b7b3a96ef66b8caf8475895
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-12-01 22:03:53 -0600

    Update Print value.py

--- Print value.py
+++ Print value.py
@@ -2,6 +2,10 @@
 # November 21, 2021
 
 # This program takes a list and prints if the value is 5 is in the list
+'''
+input: list
+define function to check whether there is 5 in the list
+call function
 
 def printValue(value, list):
     for i in list:
@@ -10,3 +14,15 @@ def printValue(value, list):
             return ans
 
 print(printValue(5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+'''
+def checkValue(list):
+    value  = 5
+    if value in list:
+        print('five is in the list')
+        print(list)
+    else:
+        print('five is not the list')
+
+list = [2, 3, 5, 1]
+checkValue(list)
+
