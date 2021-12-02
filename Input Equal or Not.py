de10b170c4e1bf6b336fb13262b917f8199753bf
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-12-01 21:56:23 -0600

    Update Input Equal or Not.py

--- Input Equal or Not.py
+++ Input Equal or Not.py
@@ -2,6 +2,10 @@
 # November 21, 2021
 
 # This program checks if user input is equal or not
+'''
+The codes for checking two numbers should be in the function.
+Place the codes for getting two inputs and calling the function after the function definition
+
 
 def equal_number():
     num1 = int(input("Enter the first number: "))
@@ -12,3 +16,14 @@ if num1 == num2:
 
 if num1 != num2:
     print("The numbers are not equal. Try again.")
+'''
+def equal_number(x,y):
+    if num1 == num2:
+        print("The two numbers are equal to each other")
+    else:
+        print("The numbers are not equal. Try again.")
+    
+    
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+equal_number(num1, num2)
